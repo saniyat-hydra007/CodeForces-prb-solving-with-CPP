bf2ba0d2b3ebd028276f6d2839b5c7c54b7aeabc
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2020-12-25 23:33:42 +0600

    .

---
+++ 266A-StoneOnTheTable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+        int a,num=0;
+        std::cin >> a;
+        string s;
+        std::cin >> s;
+        for(int i=0; i<s.size(); i++) {
+                if(s[i] == s[i+1]) {
+                        num++;
+                }
+                else
+                        continue;
+        }
+        std::cout << num << '\n';
+}
