904126e07b2fc7432d9678dd22309a09f928b9bf
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2020-12-25 15:28:28 +0600

    bit

--- NextRound.cpp
+++ NextRound.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-  int n,m,max,num=0;
-  int a[100];
-  std::cin >> n;
-  std::cin >> m;
-  for(int i=0;i<n;i++) {
-std::cin >> a[i];
-if(i+1 == m){
-max = a[i];
-}
-  }
-  for(int i=0; i<n; i++){
-    if(a[i]>=max && a[i]>0){
-    num++;
-  }
-  }
-  std::cout << num << '\n';
-  return 0;
+        int n,m,max,num=0;
+        int a[100];
+        std::cin >> n;
+        std::cin >> m;
+        for(int i=0; i<n; i++) {
+                std::cin >> a[i];
+                if(i+1 == m) {
+                        max = a[i];
+                }
+        }
+        for(int i=0; i<n; i++) {
+                if(a[i]>=max && a[i]>0) {
+                        num++;
+                }
+        }
+        std::cout << num << '\n';
+        return 0;
 }
