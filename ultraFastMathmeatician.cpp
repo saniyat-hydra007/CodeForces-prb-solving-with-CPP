ed24a4cf66243293b528e23ebac0dfecd3347201
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-04-11 12:12:12 +0600

    Create ultraFastMathmeatician.cpp

---
+++ ultraFastMathmeatician.cpp
@@ -0,0 +1,59 @@
+/**
+ * @Author: Saniyat Mushrat Lamim
+ * @Date:   03-Apr-2021
+ * @Email:  robertarmstrong096@gmail.com
+ * @Filename: ultraFastMathmeatician.cpp
+ * @Last modified by:   Saniyat Mushrat Lamim
+ * @Last modified time: 03-Apr-2021
+ */
+
+
+
+#include "bits/stdc++.h"
+
+using namespace std;
+#define el "\n"
+#define F first
+#define S second
+#define PI 3.14159265358979323846  /* pi */
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define RI(i,n) FOR(i,1,n)
+#define REP(i,n) FOR(i,0,(n)-1)
+#define SQR(x) (x)*(x)
+#define all(v)    ((v).begin()),((v).end())
+#define degreesToRadians(angleDegrees) (angleDegrees * PI / 180.0) // Converts degrees to radians.
+#define radiansToDegrees(angleRadians) (angleRadians * 180.0 / PI) // Converts radians to degrees.
+typedef long long ll;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>  pll;
+typedef long double ld;
+typedef vector<int>   vi;
+typedef vector<ll>    vll;
+typedef vector<pii>   vpii;
+typedef vector<pll>   vpll;
+const double EPS = 1e-9;
+const int N = 1e3+2, M = 3e5+5, OO = 0x3f3f3f3f;
+// int dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; // 4 Direction
+// int dx[] = {1,-1,0,0,1,1,-1,-1}, dy[] = {0,0,1,-1,1,-1,1,-1}; // 8 Direction
+// int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; // Knight Direction
+// int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; // Hexagonal Direction
+
+int main(){
+
+        string s1, s2;
+        cin >> s1 >> s2;
+        for (int i = 0; i < s1.length(); ++i)
+        {
+                if (s1[i] == s2[i])
+                {
+                        s1[i] = '0';
+                }
+                else
+                {
+                        s1[i] = '1';
+                }
+        }
+        cout << s1 << endl;
+        return 0;
+}
