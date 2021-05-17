9d9256132204b14a0b181955ec2a66bec8453c95
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-05-17 18:52:47 +0600

    Solved

---
+++ 520_Pangram.cpp
@@ -0,0 +1,58 @@
+/**
+ * @Author: Saniyat Mushrat Lamim
+ * @Date:   17-May-2021
+ * @Email:  robertarmstrong096@gmail.com
+ * @Filename: 520_Pangram.cpp
+ * @Last modified by:   Saniyat Mushrat Lamim
+ * @Last modified time: 17-May-2021
+ */
+
+
+
+#include "bits/stdc++.h"
+
+using namespace std;
+#define el "\n"
+#define F first
+#define S second
+#define PI 3.14159265358979323846  /* pi */
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define RI(i,n) FOR(i,1,n)
+#define REP(i,n) FOR(i,0,(n)-1)
+#define SQR(x) (x)*(x)
+#define all(v)    ((v).begin()),((v).end())
+#define degreesToRadians(angleDegrees) (angleDegrees * PI / 180.0) // Converts degrees to radians.
+#define radiansToDegrees(angleRadians) (angleRadians * 180.0 / PI) // Converts radians to degrees.
+typedef long long ll;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>  pll;
+typedef long double ld;
+typedef vector<int>   vi;
+typedef vector<ll>    vll;
+typedef vector<pii>   vpii;
+typedef vector<pll>   vpll;
+const double EPS = 1e-9;
+const int N = 1e3+2, M = 3e5+5, OO = 0x3f3f3f3f;
+// int dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; // 4 Direction
+// int dx[] = {1,-1,0,0,1,1,-1,-1}, dy[] = {0,0,1,-1,1,-1,1,-1}; // 8 Direction
+// int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; // Knight Direction
+// int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; // Hexagonal Direction
+
+
+int main(){
+        int n;
+        string s;
+        cin >> n;
+        cin >> s;
+        set<char> hash;
+        for (int i = 0; i < s.length(); ++i) {
+                char c = tolower(s[i]);
+                hash.insert(c);
+        }
+        if (hash.size() == 26) {
+                cout << "YES";
+        } else
+                cout << "NO";
+        return 0;
+}
