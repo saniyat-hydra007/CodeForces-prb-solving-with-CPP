e40a92933dccd704a33acda1a263b3a59e5fa85a
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-06-09 00:31:39 +0600

    Done

---
+++ 996_HitTheLottery.cpp
@@ -0,0 +1,63 @@
+/**
+ * @Author: Saniyat Mushrat Lamim
+ * @Date:   08-Jun-2021
+ * @Email:  robertarmstrong096@gmail.com
+ * @Filename: 996_HitTheLottery.cpp
+ * @Last modified by:   Saniyat Mushrat Lamim
+ * @Last modified time: 09-Jun-2021
+ */
+
+
+
+#include "bits/stdc++.h"
+#include <iostream>
+
+using namespace std;
+#define el "\n"
+#define F first
+#define S second
+#define PI 3.14159265358979323846  /* pi */
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define RI(i,n) FOR(i,1,n)
+#define REP(i,n) FOR(i,0,(n)-1)
+#define SQR(x) (x)*(x)
+#define all(v)    ((v).begin()),((v).end())
+#define degreesToRadians(angleDegrees) (angleDegrees * PI / 180.0) // Converts degrees to radians.
+#define radiansToDegrees(angleRadians) (angleRadians * 180.0 / PI) // Converts radians to degrees.
+typedef long long ll;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>  pll;
+typedef long double ld;
+typedef vector<int>   vi;
+typedef vector<ll>    vll;
+typedef vector<pii>   vpii;
+typedef vector<pll>   vpll;
+const double EPS = 1e-9;
+const int N = 1e3+2, M = 3e5+5, OO = 0x3f3f3f3f;
+// int dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; // 4 Direction
+// int dx[] = {1,-1,0,0,1,1,-1,-1}, dy[] = {0,0,1,-1,1,-1,1,-1}; // 8 Direction
+// int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; // Knight Direction
+// int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; // Hexagonal Direction
+
+int main()
+{
+        int a;
+        cin>>a;
+
+        int ans = a/100;
+        int left = a%100;
+
+        ans+= left/20;
+        left= left%20;
+
+        ans+= left/10;
+        left = left%10;
+
+        ans+= left/5;
+        left=left%5;
+
+        ans+=left/1;
+
+        cout<<ans;
+}
